a335851402d962043d4a98cc0d638ace0e1163c2
henokmuller <henokmullawshum@gmail.com>
2026-08-16 13:15:41 +0300

    feat: enhance note creation and theme toggle functionality

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
 class NOTE_FACTORY {
   note;
   date;
-  constructor(note = "12 days ago", date) {
+  constructor(note, date) {
     this.note = note;
     this.date = date;
   }
 }
 
+const NoteStorage = [];
+
 function store() {
-  const today = new Date();
-  const user = new NOTE_FACTORY(noteinput.value, today.getFullYear);
-  console.log(user.date);
+  const user = new NOTE_FACTORY(noteinput.value, today);
+  NoteStorage.push(user);
+  makeCard();
 }
 
-function updatenote() {
-  console.log(noteinput.value);
+function makeCard() {
+  const notesList = document.getElementById("notes-list");
+  const noteCard = document.createElement("article");
+  const noteContent = document.createElement("div");
+  const paragraph = document.createElement("p");
+  const noteMeta = document.createElement("div");
+  const noteactions = document.createElement("div");
+
+  const emptyState = document.getElementById("empty-state");
+  paragraph.textContent = NoteStorage.pop().note;
+
+  if (!!emptyState) emptyState.remove();
+
+  noteCard.classList.add("note-card");
+  noteContent.classList.add("note-content");
+  noteMeta.classList.add("note-meta");
+
+  notesList.appendChild(noteCard);
+  noteCard.appendChild(noteContent);
+  noteContent.appendChild(paragraph);
 }
 
+function newTheme() {
+  body.classList.toggle("dark-theme");
+
+  changeTheme.textContent == "☀️"
+    ? (changeTheme.textContent = "🌙")
+    : changeTheme.textContent == "🌙"
+    ? (changeTheme.textContent = "☀️")
+    : (changeTheme.textContent = "☀️");
+}
+
+const today = new Date();
 const noteinput = document.getElementById("note-input");
-
 const saveNotes = document.querySelector("#save-note-btn");
+const changeTheme = document.getElementById("theme-toggle");
+const body = document.querySelector("body");
 
-document.addEventListener("click", store(saveNotes));
-document.addEventListener("focusout", updatenote(noteinput));
+changeTheme.addEventListener("click", newTheme);
+saveNotes.addEventListener("click", store);
